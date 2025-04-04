6a9681df21eb6ae2265e7517ad594ba849aa906a
admin <admin@admins-MacBook-Pro.local>
2025-04-04 20:49:24 +0300

    back

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.plugins.push(
-        new MiniCssExtractPlugin({
-          filename: 'static/css/[name].[contenthash].css',
-          chunkFilename: 'static/css/[id].[contenthash].css',
-        })
-      );
-    }
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
+
+
+
+
