e317228be747aa22e40e443c52476d08e2e42c19
admin <admin@admins-MacBook-Pro.local>
2025-04-04 19:52:35 +0300

    another error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'static/css/[name].[contenthash].css',
+          chunkFilename: 'static/css/[id].[contenthash].css',
+        })
+      );
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
-
-
-
